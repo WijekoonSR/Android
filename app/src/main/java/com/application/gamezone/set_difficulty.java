f8c1879e1504e8beddc96da47fb9664336c13ec6
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-08-04 06:05:50 +0530

    Edited difficulty UI

--- app/src/main/java/com/application/gamezone/set_difficulty.java
+++ app/src/main/java/com/application/gamezone/set_difficulty.java
@@ -3,15 +3,12 @@ package com.application.gamezone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-//    Button  button = (Button)Butto
+public class set_difficulty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_set_difficulty);
     }
 }
